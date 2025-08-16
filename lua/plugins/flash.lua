41977b0f307c119e3636c063735cbe37802d837d
DavoDev-Hub <juanpajmz13@gmail.com>
2025-08-15 18:19:04 -0600

    pluggins fix

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,13 +1,32 @@
 return {
-	"folke/flash.nvim",
-	event = "VeryLazy",
-	opts = {},
-  -- stylua: ignore
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {},
   keys = {
-    { "zk",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-    { "Zk",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-    { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-    { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    {
+      "<leader>jj",
+      function() require("flash").jump() end,
+      desc = "Flash",
+      mode = { "n", "x", "o" }
+    },
+
+    {
+      "r",
+      function() require("flash").remote() end,
+      desc = "Remote Flash",
+      mode = "o"
+    },
+    {
+      "R",
+      function() require("flash").treesitter_search() end,
+      desc = "Treesitter Search",
+      mode = { "o", "x" }
+    },
+    {
+      "<C-s>",
+      function() require("flash").toggle() end,
+      desc = "Toggle Flash Search",
+      mode = "c"
+    },
   },
 }
